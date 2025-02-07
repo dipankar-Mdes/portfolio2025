ac2785114b79321fb5ac2f76065dbba87873fd39
Dipankar Mdes <roy07dipankar@gmail.com>
2025-02-08 00:50:53 +0530

    all files

--- src/Components/Artworks.jsx
+++ src/Components/Artworks.jsx
@@ -3,6 +3,22 @@ import Navbar from './Navbar'
 import Below from './Below'
 import TiltedCard from './reactbits/Components/TiltedCard/TiltedCard'
 import Masonry from 'react-masonry-css'
+import bagbakri from '../Resources/Images/bag-bakri.png'
+import bihu from '../Resources/Images/bihu.jpg'
+import defoe from '../Resources/Images/defoe.png'
+import enho from '../Resources/Images/en-ho.png'
+import exp from '../Resources/Images/expression.jpg'
+import ghar from '../Resources/Images/ghar.png'
+import greek from '../Resources/Images/greek-statue.png'
+import holy from '../Resources/Images/holy.jpeg'
+import hunk from '../Resources/Images/hunk.jpeg'
+import kids from '../Resources/Images/kids.jpg'
+import me from '../Resources/Images/me--.jpeg'
+import guy from '../Resources/Images/random-guy.png'
+import rezaaa from '../Resources/Images/rezaaa.png'
+import wings from '../Resources/Images/wings-of-justice.jpeg'
+import apple from '../Resources/Images/women-apple.jpeg'
+import pin from '../Resources/Images/pintrest-boyfriend.jpeg'
 
 
 
@@ -26,354 +42,52 @@ function Artworks() {
                 columnClassName="my-masonry-grid_column"
             >
                 <div>
-                <TiltedCard
-                        imageSrc="/src/Resources/Images/bag-bakri.png"
-                        altText="Cuppahtee"
-                        captionText="Cuppahtee"
-                        containerHeight="300px"
-                        containerWidth="300px"
-                        imageHeight="300px"
-                        imageWidth="300px"
-                        rotateAmplitude={12}
-                        scaleOnHover={1.2}
-                        showMobileWarning={false}
-                        showTooltip={true}
-                        displayOverlayContent={false}
-                        // className="break-inside-avoid"
-                        overlayContent={
-                        <p className="tilted-card-demo-text">
-                            Cuppahtee
-                        </p>
-                        }   
-                />
+                    <img className='w-[300px]' src={bagbakri} alt="" />
                 </div>
                 <div>
-                <TiltedCard
-                        imageSrc="/src/Resources/Images/bihu.jpg"
-                        altText="bihu"
-                        captionText="bihu"
-                        containerHeight="400px"
-                        containerWidth="300px"
-                        imageHeight="400px"
-                        imageWidth="300px"
-                        rotateAmplitude={12}
-                        scaleOnHover={1.2}
-                        showMobileWarning={false}
-                        showTooltip={true}
-                        displayOverlayContent={false}
-                        // className="break-inside-avoid"
-                        overlayContent={
-                        <p className="tilted-card-demo-text">
-                            bihu
-                        </p>
-                        }   
-                />
+                    <img className='w-[300px]' src={bihu} alt="" />
                 </div>
                 <div>
-                <TiltedCard
-                        imageSrc="/src/Resources/Images/defoe.png"
-                        altText="defoe"
-                        captionText="defoe"
-                        containerHeight="400px"
-                        containerWidth="300px"
-                        imageHeight="400px"
-                        imageWidth="300px"
-                        rotateAmplitude={12}
-                        scaleOnHover={1.2}
-                        showMobileWarning={false}
-                        showTooltip={true}
-                        displayOverlayContent={false}
-                        // className="break-inside-avoid"
-                        overlayContent={
-                        <p className="tilted-card-demo-text">
-                            defoe
-                        </p>
-                        }   
-                />
+                <img className='w-[300px]' src={defoe} alt="" />
                 </div>
                 <div>
-                <TiltedCard
-                        imageSrc="/src/Resources/Images/en-ho.png"
-                        altText="en-ho"
-                        captionText="en-ho"
-                        containerHeight="318px"
-                        containerWidth="300px"
-                        imageHeight="318px"
-                        imageWidth="300px"
-                        rotateAmplitude={12}
-                        scaleOnHover={1.2}
-                        showMobileWarning={false}
-                        showTooltip={true}
-                        displayOverlayContent={false}
-                        // className="break-inside-avoid"
-                        overlayContent={
-                        <p className="tilted-card-demo-text">
-                            en-ho
-                        </p>
-                        }   
-                />
+                    <img className='w-[300px]' src={enho} alt="" />
                 </div>
                 <div>
-                <TiltedCard
-                        imageSrc="/src/Resources/Images/expression.jpg"
-                        altText="Expression"
-                        captionText="Expression"
-                        containerHeight="400px"
-                        containerWidth="300px"
-                        imageHeight="400px"
-                        imageWidth="300px"
-                        rotateAmplitude={12}
-                        scaleOnHover={1.2}
-                        showMobileWarning={false}
-                        showTooltip={true}
-                        displayOverlayContent={false}
-                        // className="break-inside-avoid"
-                        overlayContent={
-                        <p className="tilted-card-demo-text">
-                            Expression
-                        </p>
-                        }   
-                />
+                    <img className='w-[300px]' src={exp} alt="" />
                 </div>
                 <div>
-                <TiltedCard
-                        imageSrc="/src/Resources/Images/ghar.png"
-                        altText="ghar"
-                        captionText="ghar"
-                        containerHeight="419px"
-                        containerWidth="300px"
-                        imageHeight="419px"
-                        imageWidth="300px"
-                        rotateAmplitude={12}
-                        scaleOnHover={1.2}
-                        showMobileWarning={false}
-                        showTooltip={true}
-                        displayOverlayContent={false}
-                        // className="break-inside-avoid"
-                        overlayContent={
-                        <p className="tilted-card-demo-text">
-                            ghar
-                        </p>
-                        }   
-                />
+                    <img className='w-[300px]' src={ghar} alt="" />
                 </div>
                 <div>
-                <TiltedCard
-                        imageSrc="/src/Resources/Images/greek-statue.png"
-                        altText="greek-statue"
-                        captionText="greek-statue"
-                        containerHeight="380px"
-                        containerWidth="300px"
-                        imageHeight="380px"
-                        imageWidth="300px"
-                        rotateAmplitude={12}
-                        scaleOnHover={1.2}
-                        showMobileWarning={false}
-                        showTooltip={true}
-                        displayOverlayContent={false}
-                        // className="break-inside-avoid"
-                        overlayContent={
-                        <p className="tilted-card-demo-text">
-                            greek-statue
-                        </p>
-                        }   
-                />
+                    <img className='w-[300px]' src={greek} alt="" />
                 </div>
                 <div>
-                <TiltedCard
-                        imageSrc="/src/Resources/Images/holy.jpeg"
-                        altText="holy"
-                        captionText="holy"
-                        containerHeight="379px"
-                        containerWidth="300px"
-                        imageHeight="379px"
-                        imageWidth="300px"
-                        rotateAmplitude={12}
-                        scaleOnHover={1.2}
-                        showMobileWarning={false}
-                        showTooltip={true}
-                        displayOverlayContent={false}
-                        // className="break-inside-avoid"
-                        overlayContent={
-                        <p className="tilted-card-demo-text">
-                            holy
-                        </p>
-                        }   
-                />
+                    <img className='w-[300px]' src={holy} alt="" />
                 </div>
                 <div>
-                <TiltedCard
-                        imageSrc="/src/Resources/Images/hunk.jpeg"
-                        altText="hunk"
-                        captionText="hunk"
-                        containerHeight="400px"
-                        containerWidth="300px"
-                        imageHeight="400px"
-                        imageWidth="300px"
-                        rotateAmplitude={12}
-                        scaleOnHover={1.2}
-                        showMobileWarning={false}
-                        showTooltip={true}
-                        displayOverlayContent={false}
-                        // className="break-inside-avoid"
-                        overlayContent={
-                        <p className="tilted-card-demo-text">
-                            hunk
-                        </p>
-                        }   
-                />
+                    <img className='w-[300px]' src={hunk} alt="" />
                 </div>
                 <div>
-                <TiltedCard
-                        imageSrc="/src/Resources/Images/kids.jpg"
-                        altText="kids"
-                        captionText="kids"
-                        containerHeight="225px"
-                        containerWidth="300px"
-                        imageHeight="225px"
-                        imageWidth="300px"
-                        rotateAmplitude={12}
-                        scaleOnHover={1.2}
-                        showMobileWarning={false}
-                        showTooltip={true}
-                        displayOverlayContent={false}
-                        // className="break-inside-avoid"
-                        overlayContent={
-                        <p className="tilted-card-demo-text">
-                            kids
-                        </p>
-                        }   
-                />
+                    <img className='w-[300px]' src={kids} alt="" />
                 </div>
                 <div>
-                <TiltedCard
-                        imageSrc="/src/Resources/Images/me--.jpeg"
-                        altText="me--"
-                        captionText="me--"
-                        containerHeight="400px"
-                        containerWidth="300px"
-                        imageHeight="400px"
-                        imageWidth="300px"
-                        rotateAmplitude={12}
-                        scaleOnHover={1.2}
-                        showMobileWarning={false}
-                        showTooltip={true}
-                        displayOverlayContent={false}
-                        // className="break-inside-avoid"
-                        overlayContent={
-                        <p className="tilted-card-demo-text">
-                            me--
-                        </p>
-                        }   
-                />
+                    <img className='w-[300px]' src={me} alt="" />
                 </div>
                 <div>
-                <TiltedCard
-                        imageSrc="/src/Resources/Images/random-guy.png"
-                        altText="random-guy"
-                        captionText="random-guy"
-                        containerHeight="400px"
-                        containerWidth="300px"
-                        imageHeight="400px"
-                        imageWidth="300px"
-                        rotateAmplitude={12}
-                        scaleOnHover={1.2}
-                        showMobileWarning={false}
-                        showTooltip={true}
-                        displayOverlayContent={false}
-                        // className="break-inside-avoid"
-                        overlayContent={
-                        <p className="tilted-card-demo-text">
-                            random-guy
-                        </p>
-                        }   
-                />
+                    <img className='w-[300px]' src={guy} alt="" />                              
                 </div>
                 <div>
-                <TiltedCard
-                        imageSrc="/src/Resources/Images/rezaaa.png"
-                        altText="rezaaa"
-                        captionText="rezaaa"
-                        containerHeight="400px"
-                        containerWidth="300px"
-                        imageHeight="400px"
-                        imageWidth="300px"
-                        rotateAmplitude={12}
-                        scaleOnHover={1.2}
-                        showMobileWarning={false}
-                        showTooltip={true}
-                        displayOverlayContent={false}
-                        // className="break-inside-avoid"
-                        overlayContent={
-                        <p className="tilted-card-demo-text">
-                            rezaaa
-                        </p>
-                        }   
-                />
+                    <img className='w-[300px]' src={rezaaa} alt="" />
                 </div>
                 <div>
-                <TiltedCard
-                        imageSrc="/src/Resources/Images/wings-of-justice.jpeg"
-                        altText="wings-of-justice"
-                        captionText="wings-of-justice"
-                        containerHeight="400px"
-                        containerWidth="300px"
-                        imageHeight="400px"
-                        imageWidth="300px"
-                        rotateAmplitude={12}
-                        scaleOnHover={1.2}
-                        showMobileWarning={false}
-                        showTooltip={true}
-                        displayOverlayContent={false}
-                        // className="break-inside-avoid"
-                        overlayContent={
-                        <p className="tilted-card-demo-text">
-                            wings-of-justice
-                        </p>
-                        }   
-                />
+                    <img className='w-[300px]' src={wings} alt="" />
                 </div>
-                <div><TiltedCard
-                        imageSrc="/src/Resources/Images/women-apple.jpeg"
-                        altText="women-apple"
-                        captionText="women-apple"
-                        containerHeight="400px"
-                        containerWidth="300px"
-                        imageHeight="400px"
-                        imageWidth="300px"
-                        rotateAmplitude={12}
-                        scaleOnHover={1.2}
-                        showMobileWarning={false}
-                        showTooltip={true}
-                        displayOverlayContent={false}
-                        // className="break-inside-avoid"
-                        overlayContent={
-                        <p className="tilted-card-demo-text">
-                            women-apple
-                        </p>
-                        }   
-                /></div>
                 <div>
-                <TiltedCard
-                        imageSrc="/src/Resources/Images/pintrest-boyfriend.jpeg"
-                        altText="pintrest-boyfriend"
-                        captionText="pintrest-boyfriend"
-                        containerHeight="452px"
-                        containerWidth="300px"
-                        imageHeight="452px"
-                        imageWidth="300px"
-                        rotateAmplitude={12}
-                        scaleOnHover={1.2}
-                        showMobileWarning={false}
-                        showTooltip={true}
-                        displayOverlayContent={false}
-                        // className="break-inside-avoid"
-                        overlayContent={
-                        <p className="tilted-card-demo-text">
-                            pintrest-boyfriend
-                        </p>
-                        }   
-                />
+                    <img className='w-[300px]' src={apple} alt="" />
+                </div>
+                <div>
+                    <img className='w-[300px]' src={pin} alt="" />
                 </div>
             </Masonry >
         </div>
